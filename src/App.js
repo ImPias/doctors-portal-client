a959f04b1ffeb00b1b45298f25bf0907e8eae12d
impias <kazipias123@gmail.com>
2020-10-16 02:26:45 +0600

    Client-Side Module-54

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home/Home';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
